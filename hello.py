25cef192d9be3c4d69a3ff10054331e04879c257
Phill Conrad <pconrad@ucsd.edu>
2015-08-24 14:05:55 -0700

    Demo of simple three page structure

---
+++ hello.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def renderMain():
+    return render_template('home.html')
+
+@app.route('/page1')
+def renderPage1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def renderPage2():
+    return render_template('page2.html')
+    
+if __name__=="__main__":
+    app.run(debug=False,host="0.0.0.0",port=54321)
