701040ba61e620a9a1322b8d62eb2f80c4cfd3da
Phill Conrad <spis16t3@ieng6-240.ucsd.edu>
2016-08-15 13:47:07 -0700

    bye bye useless file

--- tempForm.py
+++
@@ -1,6 +0,0 @@
-from flask.ext.wtf import Form
-from wtforms import FloatField
-from wtforms.validators import DataRequired
-
-class TempForm(Form):
-    fTemp = FloatField('fTemp',validators=[DataRequired()])
